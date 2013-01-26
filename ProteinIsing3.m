0957b167eb6b28f39c05f1d84fa32b943617455c
joe isaacson <isaacson.joe@gmail.com>
2013-01-26 17:47:21 -0500

    Added matlab code

---
+++ ProteinIsing3.m
@@ -0,0 +1,244 @@
+function ProteinIsing3(A0,T,tf)
+
+%A0 should be square
+A = A0;
+L = length(A);
+J0 = sparse(L^2,L^2);
+
+%initialize J0 such that ALL adjacent Amino acids have a HUGE
+%energy penality for switching (aka AA bonds are never broken)
+
+%find all amino acids
+x = [find(A==1);find(A==-1)];
+%for each amino acid, initially set J0 to 1/2 for every neighbor 
+for(i=1:length(x))
+    J0(x(i),x(i)-1)=1/2;
+    J0(x(i)-1,x(i))=1/2;
+    
+    J0(x(i),x(i)+1)=1/2;
+    J0(x(i)+1,x(i))=1/2;
+    
+    J0(x(i),x(i)-L)=1/2;
+    J0(x(i)-L,x(i))=1/2;
+    
+    J0(x(i),x(i)+L)=1/2;
+    J0(x(i)+L,x(i))=1/2;
+    
+    J0(x(i),x(i)-L-1)=1/2;
+    J0(x(i)-L-1,x(i))=1/2;
+    
+    J0(x(i),x(i)-L+1)=1/2;
+    J0(x(i)-L+1,x(i))=1/2;
+    
+    J0(x(i),x(i)+L-1)=1/2;
+    J0(x(i)+L-1,x(i))=1/2;
+    
+    J0(x(i),x(i)+L+1)=1/2;
+    J0(x(i)+L+1,x(i))=1/2;
+end
+%for each amino acid, set J0 to be 5000 if your neighbor is another aa
+for(i=1:length(x))
+    %n1 = index in A of one AA
+    n1 = x(i);
+    %find all neighbors of n1
+    nhbrs = [x(i)-L-1:x(i)-L+1,x(i)-1,x(i)+1,x(i)+L-1:x(i)+L+1];
+    idx=1;
+    %for each neighbor of n1
+    for(j=1:length(nhbrs))
+        %if the neighbor is an amino acid
+        if(A(nhbrs(j))==1 || A(nhbrs(j))==-1)
+            %mark it
+            n2(idx) = nhbrs(j);
+            idx = idx+1;
+        end
+    end
+    %for each marked neighbor, change J0 from 1/2 -> 5000
+    for(j=1:length(n2))
+        J0(n1,n2)=5000;
+    end
+end
+
+
+
+for(tStep=1:tf)
+    %find all amino acids
+    x = [find(A==1);find(A==-1)];
+    %randomly pick one to be flipped
+    i = x(randi(length(x),1));      %i = random index in A corresponding to a +/- 1
+    [ix,iy] = ind2sub(size(A),i);
+    
+
+    
+
+    %if this amino acid is not near an edge
+    if(ix>=3 && ix<=L-2 && iy>=3 && iy<=L-2)
+        %find the 8 indices in A corresponding to the 8 nhbrs of i
+        nhbrs = [i-L,i-1,i+1,i+L];
+        zeroEntries = find(A==0);
+        
+        %randomly pick a neighboring zero, j, to exchange with
+        %(only exchanges AA with 0s to preserve AA order: prevent
+        %adjacent AAs from swapping)
+        idx=1;
+        zeroNhbrs = inf;
+        for(k = 1:length(zeroEntries))
+            if(any(any(zeroEntries(k) == nhbrs)))
+                zeroNhbrs(idx)=nhbrs(find(zeroEntries(k) == nhbrs));
+                idx=idx+1;
+            end
+        end
+        if(zeroNhbrs(1)==inf)
+            clear zeroNhbrs;
+            continue;
+        end
+        r = randi(length(zeroNhbrs),1); %random index in nhbrs
+        j = zeroNhbrs(r);  % j = random index in A corresponding to 0 neighbor of i
+        nhbrs = [(i-L-1:i-L+1)',(i-1:i+1)',(i+L-1:i+L+1)'];
+        i
+        j
+        
+        %construct matrix of 25 neighbors about i
+        nhbrs2 = [(i-2*L-2:i-2*L+2)',(i-L-2:i-L+2)',(i-2:i+2)',(i+L-2:i+L+2)',(i+2*L-2:i+2*L+2)'];
+        %ai = submatrix of A: the 25 nhbrs about i
+        ai = A(nhbrs2);
+        %swap i,j to make af
+        A2 = A;
+        A2(i) = 0;
+        A2(j) = A(i);
+        af = A2(nhbrs2);
+        %swap J(i,j)s
+        J = J0;
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %i,j = indices in A that were switched
+        
+        %initially set J to be 1/2 for all neighbors of i and j
+        for(idx=[i,j])
+            J(idx,idx-1)=1/2;
+            J(idx-1,idx)=1/2;
+
+            J(idx,idx+1)=1/2;
+            J(idx+1,idx)=1/2;
+
+            J(idx,idx-L)=1/2;
+            J(idx-L,idx)=1/2;
+
+            J(idx,idx+L)=1/2;
+            J(idx+L,idx)=1/2;
+
+            J(idx,idx-L-1)=1/2;
+            J(idx-L-1,idx)=1/2;
+
+            J(idx,idx-L+1)=1/2;
+            J(idx-L+1,idx)=1/2;
+
+            J(idx,idx+L-1)=1/2;
+            J(idx+L-1,idx)=1/2;
+
+            J(idx,idx+L+1)=1/2;
+            J(idx+L+1,idx)=1/2;
+        end
+        %now update J
+        nhbrs = [j-L-1:j-L+1,j-1,j+1,j+L-1:j+L+1];
+        idx=1;
+        for(k=1:length(nhbrs))
+            if(A2(nhbrs(k))==1 || A2(nhbrs(k))==-1)
+                n2(idx) = nhbrs(k);
+                idx = idx+1;
+            end
+        end
+        %n2 = vector of indices in A corresponding to amino acids
+        %neighboring j
+        for(k=1:length(n2))
+            %only if i,j were originally neighbors
+            if(J0(i,n2)==5000)
+                J(j,n2)=5000;
+                J(n2,j)=5000;
+            else
+                J(j,n2)=10;
+                J(n2,j)=10;
+            end
+        end
+        
+        nhbrs = [i-L-1:i-L+1,i-1,i+1,i+L-1:i+L+1];
+        idx=1;
+        for(k=1:length(nhbrs))
+            if(A2(nhbrs(k))==0)
+                n2(idx) = nhbrs(k);
+                idx = idx+1;
+            end
+        end
+        %n2 = vector of indices in A corresponding to amino acids
+        %neighboring j
+        for(k=1:length(n2))
+            %only if i,j were originally neighbors
+            J(i,n2)=0;
+            J(n2,i)=0;
+        end
+        
+        
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%         i
+        dE = h(af,J,nhbrs2)-h(ai,J0,nhbrs2);
+        if(dE<=0)
+            A=A2;
+            J0=J;
+        elseif(rand()<exp(-dE/T))
+            A=A2;
+            J0=J;
+        else
+            dE
+        end
+        
+        
+        toPlot1 = A;
+        toPlot2 = A;
+        toPlot1(toPlot1<0) = 0;
+        toPlot2(toPlot2>0) = 0;
+        spy(toPlot1);
+        legend('positive')
+        hold on;
+        spy(toPlot2,'r');
+        hold off;
+        drawnow;
+        pause(.1)
+        
+    end
+%     pause(.1)
+end
+
+end
+
+function e = h(a,J,nhbrsInA)
+    L = 5;
+    nhbrs = [2:4,7:9,12:14,17:19];
+    e = 0;
+    for(i = nhbrs)
+%         J(nhbrsInA(i),nhbrsInA(i-1))
+%         J(nhbrsInA(i),nhbrsInA(i+L))
+%         J(nhbrsInA(i),nhbrsInA(i+L-1))
+        e = e+J(nhbrsInA(i),nhbrsInA(i-1))*((a(i)==a(i-1))+(a(i)==-a(i-1)));
+        e = e+J(nhbrsInA(i),nhbrsInA(i+L))*((a(i)==a(i+L))+(a(i)==-a(i+L)));
+        e = e+J(nhbrsInA(i),nhbrsInA(i+L-1))*((a(i)==a(i+L-1))+(a(i)==-a(i+L-1)));
+        e = e+J(nhbrsInA(i),nhbrsInA(i+L+1))*((a(i)==a(i+L+1))+(a(i)==-a(i+L+1)));
+    end
+    for(i=[1,6,11,16])
+        e = e+J(nhbrsInA(i),nhbrsInA(i+L))*((a(i)==a(i+L))+(a(i)==-a(i+L)));
+        e = e+J(nhbrsInA(i),nhbrsInA(i+L+1))*((a(i)==a(i+L+1))+(a(i)==-a(i+L+1)));
+    end
+    for(i=[5,10,15,20])
+        e = e+J(nhbrsInA(i),nhbrsInA(i-1))*((a(i)==a(i-1))+(a(i)==-a(i-1)));
+        e = e+J(nhbrsInA(i),nhbrsInA(i+L))*((a(i)==a(i+L))+(a(i)==-a(i+L)));
+        e = e+J(nhbrsInA(i),nhbrsInA(i+L-1))*((a(i)==a(i+L-1))+(a(i)==-a(i+L-1)));
+    end
+    for(i=22:25)
+        e = e+J(nhbrsInA(i),nhbrsInA(i-1))*((a(i)==a(i-1))+(a(i)==-a(i-1)));
+    end
+    %need energy at boundaries
+    e = -e;
+%     nhbrsInA
+%     a
+%     e
+%     pause()
+end
---
+++ ProteinIsing4.m
@@ -0,0 +1,152 @@
+function ProteinIsing4(Seq,T,tf)
+
+    %Construct matrix A0
+    A0 = sparse(length(Seq)+10,length(Seq)+10);
+%     A0(floor(length(A0)/2),5:4+length(Seq))=Seq;
+    A0(floor(length(A0)/2),5:2:4+length(Seq))=Seq(1:2:end);
+    A0(floor(length(A0)/2)-1,6:2:4+length(Seq))=Seq(2:2:end);
+    %A0 should be square
+    A = A0;
+    L = length(A);
+    J0 = sparse(L^2,L^2);
+    Js = 10;
+    %initialize J0 such that ALL adjacent Amino acids have a HUGE
+    %energy penality for switching (aka AA bonds are never broken)
+
+    %find all amino acids
+    x = [find(A==1);find(A==-1)];
+
+    %for each amino acid, set J0 to be 5000 if your neighbor is another aa
+    for(i=x')
+        %find all neighbors of n1
+        nhbrs = [i-L-1:i-L+1,i-1,i+1,i+L-1:i+L+1];
+        clear n2;
+        idx=1;
+        %for each neighbor of n1
+        for(k=nhbrs)
+            %if the neighbor is an amino acid
+            if(A(k)==1 || A(k)==-1)
+                %mark it
+                n2(idx) = k;
+                idx = idx+1;
+            end
+        end
+        J0(i,n2)=5000;
+    end
+
+
+
+    for(tStep=1:tf)
+        %find all amino acids
+        x = [find(A==1);find(A==-1)];
+        %randomly pick one to be flipped
+        i = x(randi(length(x),1));      %i = random index in A corresponding to a +/- 1
+        [ix,iy] = ind2sub(size(A),i);
+
+
+        %if this amino acid is not near an edge
+        if(ix>=3 && ix<=L-2 && iy>=3 && iy<=L-2)
+            %find the 8 indices in A corresponding to the 8 nhbrs of i
+            nhbrsi = [(i-L-1:i-L+1)',(i-1:i+1)',(i+L-1:i+L+1)'];
+            zeroEntries = find(A==0);
+
+            %pick j - zero nhbr of i
+            idx=1;
+            zeroNhbrs = inf;
+            for(k = 1:length(zeroEntries))
+                if(any(any(zeroEntries(k) == nhbrsi)))
+                    zeroNhbrs(idx)=nhbrsi(find(zeroEntries(k) == nhbrsi));
+                    idx=idx+1;
+                end
+            end
+            if(zeroNhbrs(1)==inf)
+                clear zeroNhbrs;
+                continue;
+            end
+            r = randi(length(zeroNhbrs),1); %random index in nhbrs
+            j = zeroNhbrs(r);  % j = random index in A corresponding to 0 neighbor of i
+            
+            clear zeroNhbrs;
+
+
+            %Update J
+            J = J0;
+            nhbrsj = [j-L-1:j-L+1,j-1,j+1,j+L-1:j+L+1];
+            clear n2;
+            idx=1;
+            for(k=nhbrsj)
+                if(A(k)==1 || A(k)==-1)
+                    n2(idx) = k;
+                    idx = idx+1;
+                end
+            end
+            for(k=n2)
+                %only if i,j were originally neighbors
+                if(J0(i,k)==5000)
+                    J(j,k)=5000;
+                    J(k,j)=5000;
+                end
+            end
+            nhbrsi = [i-L-1:i-L+1,i-1,i+1,i+L-1:i+L+1];
+            clear n2;
+            idx=1;
+            for(k=nhbrsi)
+                J(i,k)=0;
+                J(k,i)=0;
+            end
+
+            %Calc Energies
+            dEs = 2*Js*(h(A,i,nhbrsi)+h(A,j,nhbrsj));     %change in spin energies
+            dEa = h2(J0,i,nhbrsi)-h2(J,j,nhbrsj);         %energy penality for breaking AA bond
+            dE = dEs+dEa;
+            if(dE<=0)
+                A(j) = A(i);
+                A(i) = 0;
+                J0=J;
+%                 PlotMy(A);
+            elseif(rand()<exp(-dE/T))
+                A(j) = A(i);
+                A(i) = 0;
+                J0 = J;
+%                 PlotMy(A);
+            else
+                %try recurstion to move all nhbrs at once
+            end
+
+        else
+%             out = 'WARNING HIT EDGE OF MATRIX!'
+%             PlotMy(A);
+%             pause()
+        end
+        
+    end
+    PlotMy(A);
+end
+
+function e = h(A,i,nhbrs)
+    e = 0;
+    for(j = nhbrs)
+        e = e+A(i)*A(j);
+    end
+end
+
+function e = h2(J,i,nhbrs)
+    e = 0;
+    for(j=nhbrs)
+        e = e+J(i,j);
+    end
+end
+
+function PlotMy(A)
+    toPlot1 = A;
+    toPlot2 = A;
+    toPlot1(toPlot1<0) = 0;
+    toPlot2(toPlot2>0) = 0;
+    spy(toPlot1);
+    legend('positive')
+    hold on;
+    spy(toPlot2,'r');
+    hold off;
+    drawnow;
+    pause(.1)
+end
